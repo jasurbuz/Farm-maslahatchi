a20f2333d624de8944864a8a96d01eb8440f44cb
Jasurbek Khasanboyev <81855769+jasurbuz@users.noreply.github.com>
2021-12-04 16:50:35 +0500

    Added repository and constants

---
+++ Farm-Maslahatchi.Services/Repository/GenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using FarmMaslahatchi.Data;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Linq;
+using FarmMaslahatchi.Data.Model;
+
+namespace Farm_Maslahatchi.Services.Repository
+{
+    public class GenericRepository<T> where T : class 
+    {
+        private readonly string ID;
+        private readonly string Version;
+        private HttpClient Client;
+        public GenericRepository(string databaseId, string databaseVersion)
+        {
+            ID = databaseId;
+            Version = databaseVersion;
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("http://data.gov.uz/uz/api/v1/json/dataset/");
+        }
+        public async Task<IList<T>> GetAll()
+        {
+            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Get, $"{ID}/version/{Version}?access_key={Constants.Key}");
+            var response = await Client.SendAsync(message);
+            var @string = await response.Content.ReadAsStringAsync();
+            var res = JsonConvert.DeserializeObject<IList<T>>(@string);
+            return res;
+        }
+    }
+}
---
+++ FarmMaslahatchi.Data/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmMaslahatchi.Data
+{
+    public static class Constants
+    {
+        public static readonly string Key = "d12142b66d7096864e290bb30922158c";
+        public static readonly string DoriNarhId = "614";
+        public static readonly string DoriSinonimId = "4122";
+        public static readonly string DoriNarhVersion = "33854";
+        public static readonly string DoriSinonimVersion = "8788";
+    }
+}
