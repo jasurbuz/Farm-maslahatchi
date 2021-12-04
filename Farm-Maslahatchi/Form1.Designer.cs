321e5af636af19e8a80cf07298e4d9a80178a743
Jasurbek Khasanboyev <81855769+jasurbuz@users.noreply.github.com>
2021-12-04 17:03:05 +0500

    search example added

--- Farm-Maslahatchi/Form1.Designer.cs
+++ Farm-Maslahatchi/Form1.Designer.cs
@@ -57,12 +57,13 @@ namespace Farm_Maslahatchi
             this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.996886F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.234097F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.84224F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1266, 786);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(950, 639);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // label1
@@ -70,10 +71,11 @@ namespace Farm_Maslahatchi
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Location = new System.Drawing.Point(2, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(30, 20, 0, 0);
-            this.label1.Size = new System.Drawing.Size(306, 58);
+            this.label1.Padding = new System.Windows.Forms.Padding(22, 16, 0, 0);
+            this.label1.Size = new System.Drawing.Size(251, 47);
             this.label1.TabIndex = 3;
             this.label1.Text = "Farm Maslahatchi";
             // 
@@ -82,20 +84,22 @@ namespace Farm_Maslahatchi
             this.tableLayoutPanel1.SetColumnSpan(this.panel1, 2);
             this.panel1.Controls.Add(this.materialRaisedButton1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(382, 3);
+            this.panel1.Location = new System.Drawing.Point(287, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(881, 32);
+            this.panel1.Size = new System.Drawing.Size(661, 26);
             this.panel1.TabIndex = 4;
             // 
             // materialRaisedButton1
             // 
             this.materialRaisedButton1.Depth = 0;
             this.materialRaisedButton1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.materialRaisedButton1.Location = new System.Drawing.Point(848, 0);
+            this.materialRaisedButton1.Location = new System.Drawing.Point(636, 0);
+            this.materialRaisedButton1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
             this.materialRaisedButton1.Name = "materialRaisedButton1";
             this.materialRaisedButton1.Primary = true;
-            this.materialRaisedButton1.Size = new System.Drawing.Size(33, 32);
+            this.materialRaisedButton1.Size = new System.Drawing.Size(25, 26);
             this.materialRaisedButton1.TabIndex = 0;
             this.materialRaisedButton1.Text = "X";
             this.materialRaisedButton1.UseVisualStyleBackColor = true;
@@ -107,9 +111,10 @@ namespace Farm_Maslahatchi
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(382, 81);
+            this.panel2.Location = new System.Drawing.Point(287, 65);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(881, 42);
+            this.panel2.Size = new System.Drawing.Size(661, 35);
             this.panel2.TabIndex = 6;
             // 
             // label5
@@ -118,10 +123,11 @@ namespace Farm_Maslahatchi
             this.label5.Dock = System.Windows.Forms.DockStyle.Right;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(603, 0);
+            this.label5.Location = new System.Drawing.Point(449, 0);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Padding = new System.Windows.Forms.Padding(10, 10, 100, 0);
-            this.label5.Size = new System.Drawing.Size(278, 42);
+            this.label5.Padding = new System.Windows.Forms.Padding(8, 8, 75, 0);
+            this.label5.Size = new System.Drawing.Size(212, 34);
             this.label5.TabIndex = 3;
             this.label5.Text = "Dori narxlari";
             // 
@@ -132,9 +138,10 @@ namespace Farm_Maslahatchi
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Padding = new System.Windows.Forms.Padding(100, 10, 0, 0);
-            this.label2.Size = new System.Drawing.Size(251, 42);
+            this.label2.Padding = new System.Windows.Forms.Padding(75, 8, 0, 0);
+            this.label2.Size = new System.Drawing.Size(191, 34);
             this.label2.TabIndex = 3;
             this.label2.Text = "Sinonimlar";
             // 
@@ -142,9 +149,10 @@ namespace Farm_Maslahatchi
             // 
             this.panel3.Controls.Add(this.search_txt);
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(3, 81);
+            this.panel3.Location = new System.Drawing.Point(2, 65);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(373, 42);
+            this.panel3.Size = new System.Drawing.Size(281, 35);
             this.panel3.TabIndex = 7;
             // 
             // search_txt
@@ -152,18 +160,18 @@ namespace Farm_Maslahatchi
             this.search_txt.Dock = System.Windows.Forms.DockStyle.Fill;
             this.search_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.search_txt.Location = new System.Drawing.Point(0, 0);
+            this.search_txt.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.search_txt.Name = "search_txt";
-            this.search_txt.Size = new System.Drawing.Size(373, 41);
+            this.search_txt.Size = new System.Drawing.Size(281, 35);
             this.search_txt.TabIndex = 0;
             this.search_txt.TextChanged += new System.EventHandler(this.search_txt_TextChanged);
             // 
             // Farm_maslahatchi
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1266, 786);
+            this.ClientSize = new System.Drawing.Size(950, 639);
             this.Controls.Add(this.tableLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Farm_maslahatchi";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Farm Maslahatchi";
--- Farm-Maslahatchi/Form1.cs
+++ Farm-Maslahatchi/Form1.cs
@@ -1,4 +1,7 @@
-﻿using Farm_Maslahatchi.UI_Controls;
+﻿using Farm_Maslahatchi.Services.Repository;
+using Farm_Maslahatchi.UI_Controls;
+using FarmMaslahatchi.Data;
+using FarmMaslahatchi.Data.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,18 +16,23 @@ namespace Farm_Maslahatchi
 {
     public partial class Farm_maslahatchi : MaterialSkin.Controls.MaterialForm
     {
+        private GenericRepository<DoriNarh> Dori;
+        private GenericRepository<DoriSinonim> Sinonim;
         public Farm_maslahatchi()
         {
-            InitializeComponent(); 
+            InitializeComponent();
+            Dori = new GenericRepository<DoriNarh>(Constants.DoriNarhId, Constants.DoriNarhVersion);
+            Sinonim = new GenericRepository<DoriSinonim>(Constants.DoriSinonimId, Constants.DoriSinonimVersion);
         }
         private void Farm_maslahatchi_Load(object sender, EventArgs e)
         {
             
         }
 
-        private void search_txt_TextChanged(object sender, EventArgs e)
+        private async void search_txt_TextChanged(object sender, EventArgs e)
         {
-
+            var dorilar = await Dori.GetAll();
+            var result = dorilar.Where(p => p.DoriNomi.StartsWith(search_txt.Text));
         }
 
         private void materialRaisedButton1_Click(object sender, EventArgs e)
